542d159efc68a7e964b6c4f4d0ba18d3ba9cc6ab
xylow <gabriel_a_galvao@hotmail.com>
2018-10-19 23:16:10 +0200

    Added traceability of Neff, X and Y. Commented graphs. Generated new result graphs.

--- ParticleTemplate.m
+++ ParticleTemplate.m
@@ -8,12 +8,12 @@
 clear all;
 Z=load('map.asc');
 [I J]=size(Z);                      % Map dimensions
-figure(1);contour(Z,50);            % Level curves plotting
-set(gca,'Nextplot','replace');
+%figure(1);contour(Z,50);            % Level curves plotting
+%set(gca,'Nextplot','replace');
 
 %% section 2
 % Initialisation des param?tres
-N = 500;                % Number of time steps.
+N = 50;                % Number of time steps.
 t = 1:1:N;              % Time.
 v0=1;                   % initial speed along x1
 % x = zeros(N,2);         % Hidden states.
@@ -23,12 +23,12 @@ x(1,2) = 170;           % Initial state.
 Rreal = 10^2;           % Measurement noise real variance.
 R=10^2;                 % Measurement noise used for estimation
 Qreal = [0.1 0;0 10];   % Process noise real variance.
-Q = Qreal;              % Process noise variance used for estimation
-initVar = [0 0;0 0];    % Initial variance of the states.
-numSamples=100;         % Number of Particles per time step.
+Q = Qreal;             % Process noise in y variance used for estimation
+initVar = [100 0;0 100];    % Initial variance of the states.
+numSamples=200;         % Number of Particles per time step.
 
 %% Section 3
-% G?n?ration de la trajectoire et des mesures
+% Generation de la trajectoire et des mesures
 for t=2:N
     x(t,:)=x(t-1,:)+[v0 0]+randn(1,2)*sqrt(Qreal); % trajectory (process) (time, position)
 end
@@ -52,15 +52,18 @@ q=ones(1,numSamples);
 xu(1,:)=xu(1,:)+x(1,1);     % Creation of 100 realizations of a gaussian random var following N(x(t=1,1), initVar) --> X dimension
 xu(2,:)=xu(2,:)+x(1,2);     % Creation of 100 realizations of a gaussian random var following N(x(t=1,2), initVar) --> Y dimension
 
-hx=line(x(:,1),x(:,2),'LineStyle','-','Marker','none','Color','black');%trac? de la trajectoire compl?te
-hxpos=line(0,0,'LineStyle','none',...
-    'Marker','o','MarkerSize',7,'MarkerEdgeColor','black','MarkerFaceColor','black');%trac? de la positioncourante
-hxu=line(0,0,'LineStyle','none','Marker','o');%trac? des particules
-hxell=line(0,0,'LineStyle','-','Marker','none');%trac? de l'ellipse
+% hx=line(x(:,1),x(:,2),'LineStyle','-','Marker','none','Color','black');%trac? de la trajectoire compl?te
+% hxpos=line(0,0,'LineStyle','none',...
+%     'Marker','o','MarkerSize',7,'MarkerEdgeColor','black','MarkerFaceColor','black');%trac? de la positioncourante
+% hxu=line(0,0,'LineStyle','none','Marker','o');%trac? des particules
+% hxell=line(0,0,'LineStyle','-','Marker','none');%trac? de l'ellipse
 
 
 %% Section 5
 % Update et prediction
+Neff_hist = zeros(1,N-1);
+EstX_hist = zeros(2,N-1);
+
 for t=1:N-1
     %Predict
     %from the set of particles xu generate a new set xu
@@ -71,25 +74,25 @@ for t=1:N-1
         m(k)=interp2(Z,xu(1,k),xu(2,k));    %mesures predites pour chaque particules
     end
     %from the set of weights q compute the new set of weights q
-    sz1 = size(q)
     q=q.*exp(-1/(2*R)*(m-y(t,1).*ones(size(m))).^2)/sqrt(2*pi*R);
-    sz2 = size(q)
     [ii jj]=find(xu(1,:)>J | xu(1,:)<1 | xu(2,:)>I | xu(2,:)<1 );
     q(jj)=0; %Elimine les eventuelles particules "hors du cadre"
     q=q./sum(q);
     
     %Resampling
-    Neff=1/sum(q.^2);
-    if Neff<0.75*numSamples
+    Neff=1/sum(q.^2)
+    Neff_hist(t) = Neff;
+    if Neff<0.75*numSamples %|| isnan(Neff)
         %Resamplpling
-        method='multinomial';
+        method='uniform';
         switch method
             case 'none'
                 xur=xu;
                 q=q;
-            case 'multinomial'
+            case 'uniform'
                 [xur,q] = resample(xu,q);
-                sz3 = size(q)
+            case 'multinomial'
+                
         end
                 xu=xur;
     end
@@ -97,24 +100,44 @@ for t=1:N-1
     %Stockage dans xxu
     xxu(t,:,:)=xu;
     %mise a jour affichages
-    pause(0.001);
-    set(hxu,'Xdata',reshape(xu(1,:),1,numSamples),...
-        'Ydata',reshape(xu(2,:),1,numSamples));
-    %     set(hx,'Xdata',x(1:t,1),'Ydata',x(1:t,2));
-    set(hxpos,'Xdata',x(t,1),'Ydata',x(t,2));
+%     pause(0.001);
+%     set(hxu,'Xdata',reshape(xu(1,:),1,numSamples),...
+%         'Ydata',reshape(xu(2,:),1,numSamples));
+%     %     set(hx,'Xdata',x(1:t,1),'Ydata',x(1:t,2));
+%     set(hxpos,'Xdata',x(t,1),'Ydata',x(t,2));
     %Ellipsoid
     X0=mean(xu,2);
+    EstX_hist(:,t) = X0;
     M=(xu-X0*ones(1,length(xu)))*(xu-X0*ones(1,length(xu)))'/length(xu);
     [U,S,V]=svd(M);
     a=0:0.1:2*pi;
     ell0=[sqrt(S(1,1))*cos(a);sqrt(S(2,2))*sin(a)];
     ell=X0*ones(1,length(a))+V'*ell0;
-    set(hxell,'Xdata',ell(1,:),'Ydata',ell(2,:),'LineWidth',2,'Color','r'); 
+    %set(hxell,'Xdata',ell(1,:),'Ydata',ell(2,:),'LineWidth',2,'Color','r'); 
     
-    figure(2);
-    plot(1:numSamples, sort(q));
-    title("Particle weight in iteration "+t)
+%     figure(2);
+%     plot(1:numSamples, sort(q));
+%     title("Particle weight in iteration "+t)
     
-    sum(q)
-    end
+    sum(q);
+end
 
+figure(3)
+plot(1:N-1,Neff_hist)
+title("Effective number of particles through time")
+xlabel("time [s]")
+ylabel("Neff")
+
+figure(4)
+plot(1:N-1,EstX_hist(1,:),'color','r'); hold on;
+plot(1:N,x(:,1),'color','b')
+title("Estimation in the X axis through time")
+xlabel("time [s]")
+ylabel("X")
+
+figure(5)
+plot(1:N-1,EstX_hist(2,:),'color','r'); hold on;
+plot(1:N,x(:,2),'color','b')
+title("Estimation in the Y axis through time")
+xlabel("time [s]")
+ylabel("Y")
